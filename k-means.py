8d666193f50508d81ee2a2de0b2c92b799160459
jonnyle2 <45051055+jonnyle2@users.noreply.github.com>
2019-11-15 18:21:03 -0600

    Update k-means.py

--- k-means.py
+++ k-means.py
@@ -4,25 +4,11 @@ import math
 import numpy as np
 from sklearn.cluster import KMeans, MiniBatchKMeans
 
-
-regex_str = [r'<[^>]+>',
-    r'(?:@[\w_]+)',
-    r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)",
-    r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+',
-    r'(?:(?:\d+,?)+(?:\.?\d+)?)',
-    r"(?:[a-z][a-z'\-_]+[a-z])",
-    r'(?:[\w_]+)',]
-
-token_re = re.compile(r'(' + '|'.join(regex_str) + ')', re.VERBOSE | re.IGNORECASE)
-
-def tokenchar(s):
-    return token_re.findall(s)
-
-def preprocess(s, lowercase=True):
-    tokens = tokenchar(s)
-    if lowercase:
-        tokens = [token.lower() for token in tokens]
-    return tokens
+def preprocess(s):
+    preprocessed_s = re.sub('#', '', s.lower())
+    preprocessed_s = re.sub(r'(?:@[\w_]+)', '', preprocessed_s)
+    preprocessed_s = re.sub(r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+', ' ', preprocessed_s)
+    return preprocessed_s
 
 def jaccard(a, b):
     inter = list(set(a) & set(b))
